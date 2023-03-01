f5c0c51a2517ae4c16a7ea46e148d32f8fdd4e48
Ankit Singh <as.ankitsingh0702@gmail.com>
2023-03-01 23:00:22 +0530

    add

--- Components/Buttons.tsx
+++ Components/Buttons.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Link from "next/link";
-
-import { SiLeetcode,SiGithub,SiLinkedin,SiTwitter} from "react-icons/si";
+import {SiGithub,SiLinkedin,SiTwitter} from "react-icons/si";
 
 type props = {
   link: string;
@@ -11,7 +10,7 @@ type props = {
 const Buttons: React.FC<props> = ({ link, name }) => {
   return (
     <Link href={link} target="_blank">
-        <div className='flex flex-row items-center rounded-md text-[#b0b0b9] select-none hover:-translate-y-1 hover: brightness-200 active:scale-90 ease-linear duration-100 cursor-pointer'>
+        <div  className='flex flex-row items-center rounded-md text-[#b0b0b9] select-none hover:-translate-y-1 hover:brightness-200 active:scale-90 ease-linear duration-100 cursor-pointer'>
           <div >
             { name === 'Github' && <SiGithub size={25}  title = " Github"/> }
             { name === 'Twitter' && <SiTwitter size={25} title = "Twitter"/> }
