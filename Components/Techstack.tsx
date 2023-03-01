2f8aebe599b597300b18ada2c566ef06024e8c8f
Ankit Singh <as.ankitsingh0702@gmail.com>
2023-03-01 22:32:44 +0530

    add

--- Components/Techstack.tsx
+++ Components/Techstack.tsx
@@ -1,34 +1,70 @@
 import React from 'react'
-
+import Image from 'next/image';
 function Techstack() {
   return (
-    <div className="mt-7">
+    <div className="mt-7 ">
             <div className=' font-semibold tracking-wide  px-1 py-2 text-xl md:text-2xl text-slate-100 select-none'>
              TechStack
             </div>
             <div className=" bg-gradient-to-r from-white  to-teal-700  w-24 h-2 rounded-xl"></div>
 
-            <div className='  mt-7 gap-x-3.5 gap-y-3 flex   flex-row flex-wrap pt-1 pb-3'>
-            <div className="bg-white/5 text=[#b6b6b9] items-center px-1.5 py-2 rounded-md text-base ">JavaScript</div>
-            <div className="bg-white/5 text=[#b6b6b9] items-center px-4 py-2 rounded-md  text-base">Java</div>
-            <div className="bg-white/5 text=[#b6b6b9] items-center px-1.5 py-2 rounded-md  text-base">TypeScript</div>
-            <div className="bg-white/5 text=[#b6b6b9] items-center px-1.5 py-2 rounded-md  text-base">Node.js</div>
-            <div className="bg-white/5 text=[#b6b6b9] items-center px-1.5 py-2 rounded-md  text-base">Express.js</div>
-            <div className="bg-white/5 text=[#b6b6b9] items-center px-1.5 py-2 rounded-md  text-base">Next.js</div>
-            <div className="bg-white/5 text=[#b6b6b9] items-center px-1.5 py-2 rounded-md  text-base">React Native</div>
-            <div className="bg-white/5 text=[#b6b6b9] items-center px-2.5 py-2 rounded-md  text-base">React.js</div>
-            <div className="bg-white/5 text=[#b6b6b9] items-center px-1.5 py-2 rounded-md  text-base">Tailwind Css</div>
-            <div className="bg-white/5 text=[#b6b6b9] items-center px-1.5 py-2 rounded-md text-base">Framer Motion</div>
-            <div className="bg-white/5 text=[#b6b6b9] items-center px-1.5 py-2 rounded-md  text-base">Redux</div>
+            <div className='  mt-7 gap-x-7 gap-y-3 flex   flex-row flex-wrap pt-1 pb-3 ml-2'>
 
-            </div>
+
+              
+               <div className="  flex  bg-white/5 text=[#b6b6b9] items-center px-1.5 py-2 rounded-md text-base hover:bg-white/10 hover:shadow-surface-elevation-medium">
+               <Image src='/Tech/js.png' alt="" height={23} width={23} className='rounded-sm'/>
+               <p className='ml-2'>JavaScript</p> 
+               </div>
+
+               <div className="  flex  bg-white/5 text=[#b6b6b9] items-center px-1.5 py-2 rounded-md text-base hover:bg-white/10 hover:shadow-surface-elevation-medium">
+               <Image src='/Tech/Ts.png' alt="" height={23} width={23} className='rounded-sm'/>
+               <p className='ml-2'>Typescript</p> 
+               </div>
+
+               <div className="  flex  bg-white/5 text=[#b6b6b9] items-center px-2.5 py-2 rounded-md text-base hover:bg-white/10 hover:shadow-surface-elevation-medium">
+               <Image src='/Tech/java.png' alt="" height={23} width={23} className='rounded-sm'/>
+               <p className='ml-4'>Java</p> 
+               </div>
+
+               <div className="  flex  bg-white/5 text=[#b6b6b9] items-center px-1.5 py-2 rounded-md text-base hover:bg-white/10 hover:shadow-surface-elevation-medium">
+               <Image src='/Tech/next.png' alt="" height={23} width={23} className='rounded-sm'/>
+               <p className='ml-3'>NextJs</p> 
+               </div>
+
+               <div className="  flex  bg-white/5 text=[#b6b6b9] items-center px-1.5 py-2 rounded-md text-base hover:bg-white/10 hover:shadow-surface-elevation-medium">
+               <Image src='/Tech/node.webp' alt="" height={28} width={23} className='rounded-sm'/>
+               <p className='ml-4'>Node Js</p> 
+               </div>
+
+               <div className="  flex  bg-white/5 text=[#b6b6b9] items-center px-1.5 py-2 rounded-md text-base hover:bg-white/10 hover:shadow-surface-elevation-medium">
+               <Image src='/Tech/express.svg' alt="" height={23} width={23} className='rounded-sm'/>
+               <p className='ml-2'>Express Js</p> 
+               </div>
+
+               <div className="  flex  bg-white/5 text=[#b6b6b9] items-center px-1.5 py-2 rounded-md text-base hover:bg-white/10 hover:shadow-surface-elevation-medium">
+               <Image src='/Tech/react.svg' alt="" height={23} width={23} className='rounded-sm'/>
+               <p className='ml-3'>React</p> 
+               </div>
+
+               <div className="  flex  bg-white/5 text=[#b6b6b9] items-center px-1.5 py-2 rounded-md text-base hover:bg-white/10 hover:shadow-surface-elevation-medium">
+               <Image src='/Tech/react.svg' alt="" height={23} width={23} className='rounded-sm'/>
+               <p className='ml-2'>ReactNative</p> 
+               </div>
+
+               <div className="  flex  bg-white/5 text=[#b6b6b9] items-center px-1.5 py-2 rounded-md text-base hover:bg-white/10 hover:shadow-surface-elevation-medium">
+               <Image src='/Tech/redux.svg' alt="" height={23} width={23} className='rounded-sm'/>
+               <p className='ml-3'>Redux</p> 
+               </div>
+
+               <div className="  flex  bg-white/5 text=[#b6b6b9] items-center px-1.5 py-2 rounded-md text-base hover:bg-white/10 hover:shadow-surface-elevation-medium">
+               <Image src='/Tech/TailwindCSS.png' alt="" height={23} width={23} className='rounded-sm'/>
+               <p className='ml-2'>TailwindCss</p> 
+               </div>
+          </div>
       </div>
   )
 }
 
 export default Techstack
 
-
-// FOR IMAGE I HAVE TO GIVE OPACITY 
-// OPACITY:0 FOR NORMAL
-// OPACITY:1 FOR HOVER
